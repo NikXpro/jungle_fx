a174748ec07da1ab88467577b1138f90d19d92b1
NikX <briffanathan781@gmail.com>
2021-03-09 19:20:38 +0100

    volume voix ogmenter

--- resources/[BASE]/[ESX]/[BESOIN]/[voice]/pma-voice/client/main.lua
+++ resources/[BASE]/[ESX]/[BESOIN]/[voice]/pma-voice/client/main.lua
@@ -2,7 +2,7 @@ local Cfg = Cfg
 local currentGrid = 0
 local volume = 0.3
 if GetConvar('voice_useNativeAudio', 'false') == 'true' and GetConvarInt('voice_enableRadioSubmix', 0) == 1  then
-	volume = 0.6
+	volume = 0.7
 end
 local intialized = false
 local voiceTarget = 1
