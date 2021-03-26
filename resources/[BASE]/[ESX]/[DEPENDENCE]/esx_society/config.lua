b31f3b6ff852a6bd19222098dbb39d6b15d9f3e0
NikX <briffanathan781@gmail.com>
2021-03-26 23:03:12 +0100

    Fix action patron

--- resources/[BASE]/[ESX]/[DEPENDENCE]/esx_society/config.lua
+++ resources/[BASE]/[ESX]/[DEPENDENCE]/esx_society/config.lua
@@ -1,4 +1,5 @@
-Config                    = {}
-Config.Locale             = 'fr'
-Config.EnableESXIdentity  = true
-Config.MaxSalary          = 700
+Config = {}
+
+Config.Locale = 'fr'
+Config.EnableESXIdentity = false
+Config.MaxSalary = 3500
