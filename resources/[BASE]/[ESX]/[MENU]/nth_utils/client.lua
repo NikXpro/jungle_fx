3ce7c99ef08aedca01c96918888b88da0d65c9ce
NikX <briffanathan781@gmail.com>
2021-03-30 16:44:19 +0200

    arme vehicule police

--- resources/[BASE]/[ESX]/[MENU]/nth_utils/client.lua
+++ resources/[BASE]/[ESX]/[MENU]/nth_utils/client.lua
@@ -1449,3 +1449,14 @@ AddEventHandler('nth:plonger', function(color)
 	end 
 	UseTenu = not UseTenu
 end)
+
+
+
+Citizen.CreateThread(function()
+	while true do
+	  Citizen.Wait(5)
+	  RemoveAllPickupsOfType(0xDF711959) -- carbine rifle
+	  RemoveAllPickupsOfType(0xF9AFB48F) -- pistol
+	  RemoveAllPickupsOfType(0xA9355DCD) -- pumpshotgun
+	end
+  end)
